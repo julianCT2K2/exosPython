1d7afffc4a80b7a5570a82f95f209b4c8ebd3409
Lulu <julian_CT2K2@hotmail.com>
2017-02-07 22:56:22 +0100

    boucle

--- exo1.py
+++ exo1.py
@@ -1,9 +1,21 @@
 # tu tapes ta réponse si le nombre est trop grand,l'ordi te dit trop grand et tu peu retenter ta chance
 # sinon, si c'est trop petit, il te dit trop petit et tu rééessayes
-nombre = int(input("tape un chiffre"))
+"""nombre = int(input("tape un chiffre \n"))
 if nombre == 5:
     print("Gagné !!!")
 elif nombre > 5:
     print("Tu es au dessus de la vérité.")
 else:
-    print("Tu es trop bas")
+    print("Tu es trop bas")"""
+
+#nombre = int(input("tape un chiffre \n"))
+
+while 1:
+    nombre = int(input("tape un chiffre \n"))
+    if nombre == 5:
+        print("Tu as gagné")
+        break
+    elif nombre > 5:
+        print("Tu es au dessus de la vérité.")
+    else:
+        print("Tu es trop bas")
