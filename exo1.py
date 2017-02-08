f092d6aee70f76bf911cf4382ebbdcfd5a3a5694
Jean-Charles <vfrcarat@free.fr>
2017-02-08 21:16:48 +0100

    Je viens de rajouter le mode random !!!

--- exo1.py
+++ exo1.py
@@ -1,21 +1,41 @@
-# tu tapes ta réponse si le nombre est trop grand,l'ordi te dit trop grand et tu peu retenter ta chance
-# sinon, si c'est trop petit, il te dit trop petit et tu rééessayes
-"""nombre = int(input("tape un chiffre \n"))
+"""
+nombre = int(input("tape un chiffre \n"))
 if nombre == 5:
     print("Gagné !!!")
 elif nombre > 5:
     print("Tu es au dessus de la vérité.")
 else:
-    print("Tu es trop bas")"""
+    print("Tu es trop bas")
+"""
 
-#nombre = int(input("tape un chiffre \n"))
+"""
+
+Jeu avec une boucle, jusqu'à la bonne réponse
 
 while 1:
     nombre = int(input("tape un chiffre \n"))
-    if nombre == 5:
+    if nombre == 3:
         print("Tu as gagné")
         break
     elif nombre > 5:
+        print("Tu es au dessus de la vérité.")
+    else:
+        print("Tu es trop bas")
+"""
+
+"""
+Jeu avec une boucle, jusqu'à la bonne réponse et numéro réponse aléatoire
+"""
+
+import random
+n = random.randint(1,50)
+
+while 1:
+    nombre = int(input("tape un chiffre \n"))
+    if nombre == n:
+        print("Tu as gagné")
+        break
+    elif nombre > n:
         print("Tu es au dessus de la vérité.")
     else:
         print("Tu es trop bas")
