79edade066a77c3fd61e615fdbe6913bee61707a
vfrcarat <vfrcarat@free.fr>
2017-02-09 20:50:32 +0100

    Exo2 année bissextile Tu peux tester une année !

--- exo1.py
+++ exo1.py
@@ -29,9 +29,11 @@ Jeu avec une boucle, jusqu'à la bonne réponse et numéro réponse aléatoire
 
 import random
 n = random.randint(1,50)
-
+i = 0
 while True:
     nombre = int(input("tape un chiffre \n"))
+    if nombre == n or nombre != n:
+        print("Essai",i)
     if nombre == n:
         print("Tu as gagné")
         break
@@ -39,3 +41,4 @@ while True:
         print("Tu es au dessus de la vérité.")
     else:
         print("Tu es trop bas")
+    i += 1
