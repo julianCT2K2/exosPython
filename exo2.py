2676df2e7e4565f9d98be5b75aee4489151cdc78
vfrcarat <vfrcarat@free.fr>
2017-02-09 19:15:43 +0100

    Exo2 année bissextile Tu peux tester une année !

--- exo2.py
+++ exo2.py
@@ -1,5 +1,6 @@
 an = int(input("Veux tu savoir si une année est bissextile ?\n"))
 if an %4 == 0 and an %100 == 0:
-    print(an,"est une année bissextile")
+    if an %400 == 0:
+        print(an,"est une année bissextile.")
 else:
-    print(an,"n'est pas une année bissextile")
+    print(an,"n'est pas une année bissextile.")
