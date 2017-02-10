34a6c9ea56a07e81f620840fcdfa44fb75d68a80
vfrcarat <vfrcarat@free.fr>
2017-02-10 19:01:58 +0100

    Exo5 jeu ou il faut trouver le bon numéro avec un nombre d'essai limité en fonction de la diffilculté. Easy-10 essais Medium-5 essais Hard-3 essais

--- Exo5ChoixNiveau.py
+++ Exo5ChoixNiveau.py
@@ -1,8 +1,27 @@
+"""
+Je veux que tu affiches un menu avec un niveau de difficulté (easy, normal, hard)
+selon le choix de l'utilisateur, il aura droit à 10, 5 ou 3 essais
+"""
+
+"""
+Mise en place du nombre aléatoire pour le jeu
+"""
 import random
 n = random.randint(1,50)
-i = 0
+
+"""
+Jeu ou il faut trouver le bon nombre qui a été généré par hasard
+"""
+i = 1 # variable pour l'affichage du nbre d'essai
+level = int(input("Veuillez choisir le niveau de difficulté, 1, 2 ou 3\n")) # choix du niveau de difficulté
+while True:#boucle afin d'être sûr de choisir un niveau = à 1, 2 ou 3
+    if level < 1 or > 3:
+        print("Il y a eu une erreur lors de la saisie du niveau")
+    else:
+        print("votre niveau de difficulté est,level")
+        break
 while True:
-    nombre = int(input("tape un chiffre \n"))
+    nombre = int(input("Devine le chiffre \n"))
     if nombre == n or nombre != n:
         print("Essai",i)
     if nombre == n:
