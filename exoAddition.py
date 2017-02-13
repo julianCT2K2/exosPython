a1c93649ffbeacce23c6d9cd32ddd2f5908becc4
vfrcarat <vfrcarat@free.fr>
2017-02-13 21:55:31 +0100

    Table de multiplication

--- exoAddition.py
+++ exoAddition.py
@@ -1,7 +1,7 @@
 def addition(nb):
     i = 0
     while i < 100:
-        print(nb,"*",i + 1,"=",(i + 1) * nb)
+        print(nb,"*",i + 1,"=",nb * (i + 1))
         i += 1
 
 print(addition(int(input("Entrez un chiffre\n"))))
